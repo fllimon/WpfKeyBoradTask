0fbb19a6a88b29c8374338522814bc6247f6a867
Artur Sharaenko <49069796+fllimon@users.noreply.github.com>
2021-07-02 11:06:51 +0300

    Add NumPad + Shift Click

--- KeyboradPanel/KeyboardPanel.cs
+++ KeyboradPanel/KeyboardPanel.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
@@ -44,37 +45,123 @@ namespace KeyboradPanel
     ///     <MyNamespace:CustomControl1/>
     ///
     /// </summary>
-    public class KeyboardPanel :  StackPanel
+    public class KeyboardPanel : Panel
     {
+        public static DependencyProperty LineBreakBeforeProperty;
+
         static KeyboardPanel()
         {
-            DefaultStyleKeyProperty.OverrideMetadata(typeof(KeyboardPanel), new FrameworkPropertyMetadata(typeof(KeyboardPanel)));
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(KeyboardPanel), 
+                        new FrameworkPropertyMetadata(typeof(KeyboardPanel)));
+            FrameworkPropertyMetadata metadata = new FrameworkPropertyMetadata();
+            metadata.AffectsArrange = true;
+            metadata.AffectsMeasure = true;
+            LineBreakBeforeProperty = DependencyProperty.RegisterAttached("LineBreakBefore", typeof(bool), typeof(KeyboardPanel), metadata);
+
         }
 
-        protected override Size MeasureOverride(Size constraint)
+        public static void SetLineBreakBefore(UIElement element, bool value)
         {
+            element.SetValue(LineBreakBeforeProperty, value);
+        }
+
+        public static bool GetLineBreakBefore(UIElement element)
+        {
+            return (bool)element.GetValue(LineBreakBeforeProperty);
+        }
+
+        protected override Size MeasureOverride(Size availableSize)
+        {
+            Size currentLineSize = new Size();
             Size panelSize = new Size();
 
-            foreach (UIElement child in InternalChildren)
+            foreach (UIElement element in InternalChildren)
             {
-                child.Measure(constraint);
-                panelSize = child.DesiredSize;
+                element.Measure(availableSize);
+                Size desiredSize = element.DesiredSize;
+
+                if (GetLineBreakBefore(element) || currentLineSize.Width + desiredSize.Width > availableSize.Width)
+                {
+                    panelSize.Width = Math.Max(currentLineSize.Width, panelSize.Width);
+                    panelSize.Height += currentLineSize.Height;
+                    currentLineSize = desiredSize;
+
+                    if (desiredSize.Width > availableSize.Width)
+                    {
+                        panelSize.Width = Math.Max(desiredSize.Width, panelSize.Width);
+                        panelSize.Height += desiredSize.Height;
+                        currentLineSize = new Size();
+                    }
+                }
+                else
+                {
+                    // Keep adding to the current line.
+                    currentLineSize.Width += desiredSize.Width;
+
+                    currentLineSize.Height = Math.Max(desiredSize.Height, currentLineSize.Height);
+                }
             }
 
-            return base.MeasureOverride(panelSize);
+            panelSize.Width = Math.Max(currentLineSize.Width, panelSize.Width);
+            panelSize.Height += currentLineSize.Height;
+
+            return panelSize;
         }
 
         protected override Size ArrangeOverride(Size finalSize)
         {
-            double x = 0;
-            double y = 0;
+            int firstInLine = 0;
 
-            foreach (UIElement child in InternalChildren)
+            Size currentLineSize = new Size();
+
+            double accumulatedHeight = 0;
+
+            UIElementCollection elements = InternalChildren;
+
+            for (int i = 0; i < elements.Count; i++)
             {
-                child.Arrange(new Rect(x, y, child.DesiredSize.Width, child.DesiredSize.Height));
+
+                Size desiredSize = elements[i].DesiredSize;
+
+                if (GetLineBreakBefore(elements[i]) || currentLineSize.Width + desiredSize.Width > finalSize.Width)
+                {
+                    ArrangeLine(accumulatedHeight, currentLineSize.Height, firstInLine, i);
+
+                    accumulatedHeight += currentLineSize.Height;
+                    currentLineSize = desiredSize;
+
+                    if (desiredSize.Width > finalSize.Width)
+                    {
+                        ArrangeLine(accumulatedHeight, desiredSize.Height, i, ++i);
+                        accumulatedHeight += desiredSize.Height;
+                        currentLineSize = new Size();
+                    }
+                    firstInLine = i;
+                }
+                else
+                {
+                    currentLineSize.Width += desiredSize.Width;
+                    currentLineSize.Height = Math.Max(desiredSize.Height, currentLineSize.Height);
+                }
             }
 
-            return base.ArrangeOverride(new Size(x, finalSize.Height));
+            if (firstInLine < elements.Count)
+                ArrangeLine(accumulatedHeight, currentLineSize.Height, firstInLine, elements.Count);
+
+            return finalSize;
+        }
+
+        private void ArrangeLine(double y, double lineHeight, int start, int end)
+        {
+            double x = 0;
+            UIElementCollection children = InternalChildren;
+
+            for (int i = start; i < end; i++)
+            {
+                UIElement child = children[i];
+                child.Arrange(new Rect(x, y, child.DesiredSize.Width, lineHeight));
+                x += child.DesiredSize.Width;
+            }
         }
     }
 }
--- WpfKeyBorad/Keyboard.cs
+++ WpfKeyBorad/Keyboard.cs
@@ -21,9 +21,12 @@ namespace WpfKeyBorad
     {
         public static DependencyProperty VirtualKeyProperty = DependencyProperty.RegisterAttached("VirtualKey",
                                                              typeof(Key), typeof(Keyboard), new PropertyMetadata());
+        public const int MILTIPLY_VALUE = 2;
+        public const int MILTIPLY_SPACEKEY_VALUE = 10;
 
         private List<ButtonBase> _buttons;
         private List<Key> _keys;
+        private bool _isShiftClicked = false; 
 
         public Keyboard()
         {
@@ -39,14 +42,76 @@ namespace WpfKeyBorad
             _keys.Add(new Key(KeybordKey.KEY_U));
             _keys.Add(new Key(KeybordKey.KEY_I));
             _keys.Add(new Key(KeybordKey.KEY_O));
+            _keys.Add(new Key(KeybordKey.KEY_P));
+            _keys.Add(new Key(KeybordKey.OEM_4));
+            _keys.Add(new Key(KeybordKey.OEM_6));
+            _keys.Add(new Key(KeybordKey.BACK));
+            _keys.Add(new Key(KeybordKey.NUMPAD7));
+            _keys.Add(new Key(KeybordKey.NUMPAD8));
+            _keys.Add(new Key(KeybordKey.NUMPAD9));
+            _keys.Add(new Key(KeybordKey.TAB));
+            _keys.Add(new Key(KeybordKey.KEY_A));
+            _keys.Add(new Key(KeybordKey.KEY_S));
+            _keys.Add(new Key(KeybordKey.KEY_D));
+            _keys.Add(new Key(KeybordKey.KEY_F));
+            _keys.Add(new Key(KeybordKey.KEY_G));
+            _keys.Add(new Key(KeybordKey.KEY_H));
+            _keys.Add(new Key(KeybordKey.KEY_J));
+            _keys.Add(new Key(KeybordKey.KEY_K));
+            _keys.Add(new Key(KeybordKey.KEY_L));
+            _keys.Add(new Key(KeybordKey.OEM_1));
+            _keys.Add(new Key(KeybordKey.OEM_7));
+            _keys.Add(new Key(KeybordKey.RETURN));
+            _keys.Add(new Key(KeybordKey.NUMPAD4));
+            _keys.Add(new Key(KeybordKey.NUMPAD5));
+            _keys.Add(new Key(KeybordKey.NUMPAD6));
+            _keys.Add(new Key(KeybordKey.SHIFT));
+            _keys.Add(new Key(KeybordKey.KEY_Z));
+            _keys.Add(new Key(KeybordKey.KEY_X));
+            _keys.Add(new Key(KeybordKey.KEY_C));
+            _keys.Add(new Key(KeybordKey.KEY_V));
+            _keys.Add(new Key(KeybordKey.KEY_B));
+            _keys.Add(new Key(KeybordKey.KEY_N));
+            _keys.Add(new Key(KeybordKey.KEY_M));
+            _keys.Add(new Key(KeybordKey.OEM_COMMA));
+            _keys.Add(new Key(KeybordKey.OEM_PERIOD));
+            _keys.Add(new Key(KeybordKey.OEM_2));
+            _keys.Add(new Key(KeybordKey.OEM_5));
+            _keys.Add(new Key(KeybordKey.NUMPAD1));
+            _keys.Add(new Key(KeybordKey.NUMPAD2));
+            _keys.Add(new Key(KeybordKey.NUMPAD3));
+            //_keys.Add(new Key(KeybordKey.OEM_3));
+            _keys.Add(new Key(KeybordKey.OEM_8));
+            _keys.Add(new Key(KeybordKey.SPACE));
+            _keys.Add(new Key(KeybordKey.LEFT));
+            _keys.Add(new Key(KeybordKey.RIGHT));
+            _keys.Add(new Key(KeybordKey.OEM_PLUS));
+            _keys.Add(new Key(KeybordKey.NUMPAD0));
+            _keys.Add(new Key(KeybordKey.DECIMAL));
 
             ButtonClickCommand = new RelayCommand(o =>
             {
-                uint code = (uint)(KeybordKey)o;
+                Send((KeybordKey)o);
+            });
 
-                int scanCode = Win32.MapVirtualKey(code, 0);
+            ShiftClickCommand = new RelayCommand(o =>
+            {
+                KEYEVENTF keyFlag;
 
-                Send((ScanCode)(ushort)scanCode);
+                if (!IsShiftClicked)
+                {
+                    keyFlag = KEYEVENTF.KEYDOWN;
+
+                    IsShiftClicked = true;
+                }
+                else
+                {
+                    keyFlag = KEYEVENTF.KEYUP;
+
+                    IsShiftClicked = false;
+                }
+
+                SendShift((KeybordKey)o, keyFlag);
             });
 
             InitializeButton();
@@ -56,23 +121,109 @@ namespace WpfKeyBorad
         {
             for (int i = 0; i < _keys.Count; i++)
             {
-                _buttons.Add(new RepeatButton());
-                _buttons[i].Command = ButtonClickCommand;
+                if (_keys[i].SomeKey == KeybordKey.SHIFT)
+                {
+                    _buttons.Add(new ToggleButton());
+                    _buttons[i].Command = ShiftClickCommand;
+                }
+                else
+                {
+                    _buttons.Add(new RepeatButton());
+                    _buttons[i].Command = ButtonClickCommand;
+                }
+
+                _buttons[i].Content = GetKeyToChar(_keys[i].SomeKey, _isShiftClicked, false);
                 _buttons[i].Height = _keys[i].Height;
                 _buttons[i].Width = _keys[i].Width;
+                _buttons[i].FontSize = 18;
+
+                if (_keys[i].SomeKey == KeybordKey.OEM_PLUS)
+                {
+                    _buttons[i].Visibility = Visibility.Hidden;
+                }
+
+                if (_keys[i].SomeKey == KeybordKey.SPACE)
+                {
+                    _buttons[i].Content = _keys[i].SomeKey;
+                    _buttons[i].Width = _keys[i].Width * MILTIPLY_SPACEKEY_VALUE;
+                }
+                else if (_keys[i].SomeKey == KeybordKey.SHIFT)
+                {
+                    _buttons[i].Content = _keys[i].SomeKey;
+                    _buttons[i].Width = _keys[i].Width * MILTIPLY_VALUE + 4;
+                    _buttons[i].SetValue(KeyboardPanel.LineBreakBeforeProperty, true);
+                }
+                else if (_keys[i].SomeKey == KeybordKey.BACK)
+                {
+                    _buttons[i].Content = _keys[i].SomeKey;
+                }
+                else if (_keys[i].SomeKey == KeybordKey.RETURN)
+                {
+                    _buttons[i].Content = "Enter";
+                    _buttons[i].FontSize = 18;
+                }
+                else if (_keys[i].SomeKey == KeybordKey.LEFT)
+                {
+                    _buttons[i].Content = "<-";
+                }
+                else if (_keys[i].SomeKey == KeybordKey.RIGHT)
+                {
+                    _buttons[i].Content = "->";
+                }
+                else if (_keys[i].SomeKey == KeybordKey.MENU)
+                {
+                    _buttons[i].Content = _keys[i].SomeKey;
+                    _buttons[i].Width = _keys[i].Width * MILTIPLY_VALUE;
+                }
+                else if (_keys[i].SomeKey == KeybordKey.TAB)
+                {
+                    _buttons[i].Content = _keys[i].SomeKey;
+                    _buttons[i].Width = _keys[i].Width;
+                    _buttons[i].SetValue(KeyboardPanel.LineBreakBeforeProperty, true);
+                }
+                else if (_keys[i].SomeKey == KeybordKey.NUMPAD0)
+                {
+                    _buttons[i].Width = _keys[i].Width * MILTIPLY_VALUE;
+                }
+                else if (_keys[i].SomeKey == KeybordKey.OEM_PLUS)
+                {
+                    _buttons[i].Width = 32;
+                }
+                else
+                {
+                    _buttons[i].Width = _keys[i].Width;
+                }
+
+                if (_keys[i].SomeKey == KeybordKey.OEM_8)
+                {
+                    _buttons[i].Content = "&123";
+                    _buttons[i].SetValue(KeyboardPanel.LineBreakBeforeProperty, true);
+                }
+
                 _buttons[i].Margin = new Thickness(2);
-                _buttons[i].Content = _keys[i].SomeKey;
                 _buttons[i].Focusable = false;
 
                 SetVirtualKeyValue(_buttons[i], _keys[i]);
-
                 _buttons[i].CommandParameter = _keys[i].SomeKey;
             }
         }
-
+        
         public List<ButtonBase> Buttons => _buttons;
 
+        public bool IsShiftClicked
+        {
+            get => _isShiftClicked;
+            set
+            {
+                _isShiftClicked = value;
+
+                OnPropertyChanged();
+            }
+        }
+
         public ICommand ButtonClickCommand { get; set; }
+
+        public ICommand ShiftClickCommand { get; set; }
 
         public event PropertyChangedEventHandler PropertyChanged;
 
@@ -86,18 +237,49 @@ namespace WpfKeyBorad
             return (ButtonBase)element.GetValue(VirtualKeyProperty);
         }
 
-        public void Send(ScanCode a)
+        public void Send(KeybordKey code)
         {
             INPUT[] inputs = new INPUT[1];
             INPUT Input = new INPUT();
             Input.type = 1; // 1 = Keyboard Input
-            Input.union.ki.wScan = (uint)a;
+            Input.union.ki.wVk = (uint)code;
             Input.union.ki.dwFlags = (uint)KEYEVENTF.KEYDOWN;
             inputs[0] = Input;
 
             var result = SendInput((uint)inputs.Length, inputs, Marshal.SizeOf(typeof(INPUT)));
             var error = Marshal.GetLastWin32Error();
         }
+
+        public void SendShift(KeybordKey code, KEYEVENTF flag)
+        {
+            INPUT[] inputs = new INPUT[1];
+            INPUT Input = new INPUT();
+            Input.type = 1; // 1 = Keyboard Input
+            Input.union.ki.wVk = (uint)code;
+            Input.union.ki.dwFlags = (uint)flag;
+            inputs[0] = Input;
+
+            var result = SendInput((uint)inputs.Length, inputs, Marshal.SizeOf(typeof(INPUT)));
+            var error = Marshal.GetLastWin32Error();
+        }
+
+        private string GetKeyToChar(KeybordKey keys, bool shift, bool altGr)
+        {
+            var buf = new StringBuilder(256);
+            var keyboardState = new byte[256];
+            if (shift)
+                keyboardState[(int)KeybordKey.SHIFT] = 0xff;
+            if (altGr)
+            {
+                keyboardState[(int)KeybordKey.CONTROL] = 0xff;
+                keyboardState[(int)KeybordKey.MENU] = 0xff;
+            }
+
+            Win32.ToUnicode((uint)keys, 0, keyboardState, buf, 256, 0);
+
+            return buf.ToString();
+        }
+
 
         protected void OnPropertyChanged([CallerMemberName] string name = "")
         {
